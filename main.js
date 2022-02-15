77da1514ed6f3af50b32e67a223d67d305a868c8
Prikshit17 <94789885+Prikshit17@users.noreply.github.com>
2022-02-15 19:39:34 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+myAge = '14';
+//this is my age
+
+earlyYears = '2';
+
+earlyYears *= '10.5';
+
+laterYears = myAge - 2;
+//since we already accounted for the first 2 years we subtract 2 from myAge
+
+laterYears * 4
+//we multiply laterYears by 4 to calculate the  number of dog years
+
+myAgeInDogYears = earlyYears + laterYears;
+//we add laterYears and earlyYears to get dog age
+
+myName = "Prikshit".toLowerCase();
+//myName command turns my name in lowercase
+
+ console.log('My name is', myName, '. I am ', myAge, "years old in human years which is ", myAgeInDogYears, "years old in dog years.");
+// this code prints a line
+
